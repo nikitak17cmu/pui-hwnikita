1bd42daaced91278bdea841c13cebeef4bd42e7d
nikitak17cmu <nikitaku@andrew.cmu.edu>
2024-10-03 22:18:48 -0400

    create new js page

---
+++ solution-hw5/js/hw5.js
@@ -0,0 +1,18 @@
+// global total variable 
+// roll class
+// {
+//     constructor 
+//     calculate price function
+//     {
+//         total += price of object made
+        
+//     } 
+// }
+
+// roll objects (hard code) make the objects 
+// calculate price of individual rolls 
+
+// function to remove 
+// {
+//     total+=  price of particular object;
+// }
